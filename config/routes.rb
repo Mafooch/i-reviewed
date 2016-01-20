13e5c98bf90a85e8de0f12dd2da5b874838a91fa
Mafooch <joshuamaffuccio@gmail.com>
2016-01-19 21:25:52 -0500

    Add sessions controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  root to: "books#index"
+
   resources :books do
     resources :notes, only: [:create, :destroy]
   end
 
-  root to: "books#index"
+  resources :sessions, only: [:new, :create, :destroy]
+  get "/login" => "sessions#new", as: "login"
+  delete "/logout" => "sessions#destroy", as: "logout"
+  # rerouting because user will think in terms of login and logout, not
+  # sessions. the as lets us refer to it throughout the app however we want
+  # we no have login_path/login_url/etc..
 end
