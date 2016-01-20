69b4354ae138fb80826144490a5b2a15b1350c33
Mafooch <joshuamaffuccio@gmail.com>
2016-01-20 12:18:54 -0500

    Enable ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,10 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
+  # enable because everything between server and browser was in clear text.
+  # has_secure_password let us encrypt password in database, but messages back
+  # and forth between browser and server are not secure. need https
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
